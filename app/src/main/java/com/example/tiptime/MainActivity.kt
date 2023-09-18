1c9618c853e5ba156f5b2b805fde6fcdbffc0302
Starnoh254 <stajohn697@gmail.com>
2023-09-18 22:50:16 +0300

    Added automated test for calculateTip function

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -186,7 +187,9 @@ fun RoundTheTipRow(
  * according to the local currency.
  * Example would be "$10.00".dd
  */
-private fun calculateTip(
+
+@VisibleForTesting
+internal fun calculateTip(
     amount: Double,
     tipPercent: Double = 15.0,
     roundUp : Boolean
